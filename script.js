b1d9998442d954f56068269e2ffc480438517b5a
Daya Raj Dhungana <dayaraj.dhungana@bitskraft.com>
2024-09-11 11:35:45 +0545

    background image and footer images added

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('hidden');
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.getElementById('menuu-toggle');
+  const menu = document.getElementById('menuu');
+
+  menuButton.addEventListener('click', function() {
+      menu.classList.toggle('hidden');
+  });
+});
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 200) {
+      navbar.classList.add('bg-white');
+    } else {
+      navbar.classList.remove('bg-white');
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbarinit');
+    if (window.scrollY > 200) {
+      navbar.classList.remove('hidden');
+    } else {
+      navbar.classList.add('hidden');
+    }
+  });
